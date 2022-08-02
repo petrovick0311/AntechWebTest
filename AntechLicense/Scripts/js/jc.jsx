a6f9ca88d2451a93b7c0672861d96f38d55d1506
petrovick0311 <109626749+petrovick0311@users.noreply.github.com>
2022-08-02 18:24:54 -0500

    Actualizacion 20220802001

---
+++ AntechLicense/Scripts/js/jc.jsx
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+import { ReworthDirectory } from '@reworthrewards/reworth-directory';
+
+class DirectoryR extends React.Component {
+    render() {
+        return (
+            <ReworthDirectory
+                accentColor={'#2E58FF'}
+                lang={'ES'}
+                fontFamily={'Poppins'}
+            />
+        )
+    }
+}
+
+const Directory = () => {
+    return (
+        <ReworthDirectory
+            accentColor={'#2E58FF'}
+            lang={'ES'}
+            fontFamily={'Poppins'}
+        />
+    )
+}
+
+React.render(<Directory />, document.getElementById("Comp"))
